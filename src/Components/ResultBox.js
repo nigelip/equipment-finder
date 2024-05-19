33b4cf1137e3a61f192cebac65427e2676e7ea9c
nigelip <84696190+nigelip@users.noreply.github.com>
2024-05-19 11:21:59 +0800

    toggle the question mark at src

--- src/Components/ResultBox.js
+++ src/Components/ResultBox.js
@@ -28,12 +28,8 @@ const ResultBox = (props) => {
                   <img
                     className="eimg"
                     width={200}
-                    // src={target?.url}
-                    alt="logo"
-                    // src={`../img/${target.brand.concat(" ", target.name)}.png`}
-                    // src={`${pic?.get(target.name)}`} // the question mark here "helps" the pic appear but not all the time
-                    // the question mark here "helps" the pic appear but not all the time
-                    src="https://firebasestorage.googleapis.com/v0/b/equipment-finder-bcec5.appspot.com/o/Life%20Fitness%20Chest-Supported%20Row.png?alt=media&token=c6a28b26-ddb1-4d79-8d04-72f16da875d2"
+                    src={target.url}
+                    alt={target.name}
                   />
                 }
               >
