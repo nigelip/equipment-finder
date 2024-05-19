d5a612f6388c6b773edda45f4f6e82a9f0f37ec5
nigelip <84696190+nigelip@users.noreply.github.com>
2024-05-19 18:02:57 +0800

    jizz

--- src/Components/ResultBox.js
+++ src/Components/ResultBox.js
@@ -1,15 +1,38 @@
 import { React, useEffect, useState } from "react";
-import { List, Modal, Button } from "antd";
+import { List } from "antd";
 import { storage } from "../firebase";
 import { getDownloadURL, ref } from "firebase/storage";
 
 const ResultBox = (props) => {
   const title = props.title;
   const results = props.results;
-  console.log(results);
+  const [fetchedResults, setFetchedResults] = useState([]);
+  useEffect(() => {
+    const fetchUrls = async () => {
+      const updatedResults = await Promise.all(
+        results.map(async (item) => {
+          try {
+            const url = await getDownloadURL(
+              ref(
+                storage,
+                item.brand + " " + item.type + " " + item.name + ".png"
+              )
+            );
+            return { ...item, url };
+          } catch (error) {
+            console.error(`Failed to fetch URL for ${item.name}:`, error);
+            return { ...item, url: "https://via.placeholder.com/200" }; // Fallback URL
+          }
+        })
+      );
+      setFetchedResults(updatedResults);
+    };
+
+    fetchUrls();
+  }, [results]); // Re-run if `results` change
   return (
     <>
-      {results.length !== 0 && (
+      {fetchedResults.length !== 0 && (
         <div className="resultBox">
           {" "}
           <h1>{title}</h1>
@@ -21,7 +44,7 @@ const ResultBox = (props) => {
               width: "50%",
             }}
             bordered
-            dataSource={results}
+            dataSource={fetchedResults}
             renderItem={(target) => (
               <List.Item
                 extra={
@@ -29,13 +52,13 @@ const ResultBox = (props) => {
                     className="eimg"
                     width={200}
                     src={target.url}
-                    alt={target.name}
+                    // alt={target.name}
                   />
                 }
               >
                 <List.Item.Meta
                   title={target.name}
-                  description={<p>{target.brand}</p>}
+                  description={<p>{[target.brand, target.type]}</p>}
                 />
               </List.Item>
             )}
