6856e29ec6ab0afd3202363adff632226a64860f
isaaceng7 <110762696+isaaceng7@users.noreply.github.com>
2024-06-02 11:35:56 +0800

    Social links open on new tab

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,10 +6,10 @@ const AppFooter = () => {
       <div className="footerContent">
         <p>This is a project made for AF members who enjoy exploring the many AF gyms islandwide.</p>
         <div className="socialIcons">
-          <a href="https://twitter.com"><FaTwitter /></a>
-          <a href="https://instagram.com"><FaInstagram /></a>
-          <a href="https://linkedin.com"><FaLinkedin /></a>
-          <a href="https://tiktok.com"><FaTiktok /></a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
         </div>
       </div>
     </footer>
