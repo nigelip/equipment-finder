4f35ad7e30dea127948463c3c79d7feb3c059194
isaaceng7 <110762696+isaaceng7@users.noreply.github.com>
2024-06-04 22:08:43 +0800

    Change Twitter logo to X

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
-import { FaTwitter, FaInstagram, FaLinkedin, FaTiktok } from 'react-icons/fa';
+import { FaInstagram, FaLinkedin, FaTiktok } from 'react-icons/fa';
+import { AiOutlineX } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 const scrollToTop = () => {
@@ -18,10 +19,11 @@ const AppFooter = () => {
           <Link to="/" onClick={scrollToTop} className='web-link-btn'>Contribute</Link>      
         </div>
         <div className='social-links'>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          {/* <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a> */}
           <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://x.com" target="_blank" rel="noopener noreferrer"><AiOutlineX /></a>
+          
         </div>
       </div>
     </footer>
