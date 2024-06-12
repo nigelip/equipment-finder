039d7bdbf34d0701d849414164efbb656f8e9b7a
nigelip <84696190+nigelip@users.noreply.github.com>
2024-06-12 14:53:09 +0800

    refactoring for catalogue and gymresult

--- src/Components/Catalogue.js
+++ src/Components/Catalogue.js
@@ -1,16 +1,15 @@
-import React, { useContext, useMemo, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import { collection, onSnapshot } from "firebase/firestore";
-import { storage } from "../firebase";
-import { getDownloadURL, ref } from "firebase/storage";
+import ResultBox from "./ResultBox";
 
 const Catalogue = () => {
-  const [uniqueData, setUniqueData] = useState([]);
-  const [fetchedResults, setFetchedResults] = useState([]);
+  const [groupedData, setGroupedData] = useState([]);
 
   useEffect(() => {
     const unsub = onSnapshot(collection(db, "equipment"), (snapshot) => {
       let uniqueItems = {};
+      const groupedResults = {};
       snapshot.forEach((doc) => {
         const { name, type, brand, target } = doc.data();
         const key = `${name}_${type}_${brand}_${target}`;
@@ -19,88 +18,36 @@ const Catalogue = () => {
         }
       });
       const uniqueDataArray = Object.values(uniqueItems);
-      setUniqueData(uniqueDataArray);
+      uniqueDataArray.forEach((item) => {
+        if (!groupedResults[item.target]) {
+          groupedResults[item.target] = [];
+        }
+        groupedResults[item.target].push({ ...item });
+      });
+      setGroupedData(groupedResults);
     });
     return () => unsub();
   }, []); // Empty dependency array to run effect only once on component mount
 
-  // useEffect(() => {
-  //   const fetchUrls = async () => {
-  //     const updatedResults = await Promise.all(
-  //       uniqueData.map(async (item) => {
-  //         try {
-  //           const url = await getDownloadURL(
-  //             ref(
-  //               storage,
-  //               item.brand + " " + item.type + " " + item.name + ".png"
-  //             )
-  //           );
-  //           return { ...item, url };
-  //         } catch (error) {
-  //           return { ...item, url: "https://via.placeholder.com/200" };
-  //         }
-  //       })
-  //     );
-  //     setFetchedResults(updatedResults);
-  //     console.log(updatedResults);
-  //   };
-
-  //   fetchUrls();
-  // }, [uniqueData]);
-
-  useEffect(() => {
-    const fetchUrls = async () => {
-      const groupedResults = {}; // Object to store grouped results
-
-      await Promise.all(
-        uniqueData.map(async (item) => {
-          try {
-            const url = await getDownloadURL(
-              ref(storage, `${item.brand} ${item.type} ${item.name}.png`)
-            );
-            // If the brand key doesn't exist in groupedResults, create it with an empty array
-            if (!groupedResults[item.target]) {
-              groupedResults[item.target] = [];
-            }
-            // Push the item with its URL to the array corresponding to its brand
-            groupedResults[item.target].push({ ...item, url });
-          } catch (error) {
-            // If there's an error fetching the URL, use a placeholder image
-            if (!groupedResults[item.target]) {
-              groupedResults[item.target] = [];
-            }
-            groupedResults[item.target].push({
-              ...item,
-              url: "https://via.placeholder.com/200",
-            });
-          }
-        })
-      );
-
-      setFetchedResults(groupedResults);
-      console.log(groupedResults);
-    };
-
-    fetchUrls();
-  }, [uniqueData]);
+  // Define the desired order of targets
+  const targetOrder = [
+    "Chest",
+    "Back",
+    "Arms",
+    "Shoulders",
+    "Legs",
+    "Glutes",
+    "Core",
+    "Multipurpose",
+  ];
 
   return (
     <div>
-      {Object.keys(fetchedResults).map((target) => (
+      {targetOrder.map((target) => (
         <div key={target}>
-          <h2>{target}</h2>
-          <ul>
-            {fetchedResults[target].map((item, index) => (
-              <li key={index}>
-                Name: {item.name}, Type: {item.type}, Brand: {item.brand}
-                <img
-                  src={item.url}
-                  alt={item.name}
-                  style={{ maxWidth: "200px", maxHeight: "200px" }}
-                />
-              </li>
-            ))}
-          </ul>
+          {groupedData[target] && (
+            <ResultBox title={target} results={groupedData[target]} />
+          )}
         </div>
       ))}
     </div>
